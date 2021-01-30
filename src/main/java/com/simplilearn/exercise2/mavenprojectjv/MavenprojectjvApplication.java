5d1dfa12989af86941bb332b8137149f80ea60c4
student <hectormrg0823@gmail.com>
2021-01-30 17:00:23 +0000

    java exercise

---
+++ src/main/java/com/simplilearn/exercise2/mavenprojectjv/MavenprojectjvApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearn.exercise2.mavenprojectjv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenprojectjvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenprojectjvApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/simplilearn/exercise2/mavenprojectjv/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.simplilearn.exercise2.mavenprojectjv;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MavenprojectjvApplication.class);
+	}
+
+}
